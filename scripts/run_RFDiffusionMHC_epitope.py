c7aa37e3f9c9859cc15925e1de56cc6c069b0098
sermare <sergiomar10@berkeley.edu>
2025-08-15 01:06:10 -0700

    added plot generated and cleaned notebooks

--- scripts/run_RFDiffusionMHC_epitope.py
+++ scripts/run_RFDiffusionMHC_epitope.py
@@ -426,10 +426,10 @@ rm_aa = args.epitope_chain
 # Fixed parameters
 pdb = f"/clusterfs/nilah/sergio/RFdifussion/structures/pdb_files/{pdb.lower()}.pdb"
 
-hotspot = "A5,A9,A33,A45,A59,A66,A67,A69,A70,A73,A76,A77,A80,A81,A84,A95,A97,A114,A123,A124,A142,A147,A152,A159" #ONLY HIGH CONTACT
+# hotspot = "A5,A9,A33,A45,A59,A66,A67,A69,A70,A73,A76,A77,A80,A81,A84,A95,A97,A114,A123,A124,A142,A147,A152,A159" #ONLY HIGH CONTACT
 
 ## Added this 20th May 2025 for controls of no hotspots
-hotspot = "A1"
+hotspot = "B50"
 
 num_designs = 1
 # contigs = "A1-274/0 B1-100/0 C1-9"
